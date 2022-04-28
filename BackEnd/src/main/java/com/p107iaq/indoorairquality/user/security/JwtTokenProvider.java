56b58443e8e49bcc6740cbcd2f0864dfeedd2570
Duc Tran <s3752703@student.rmit.edu.au>
2022-04-28 23:49:32 +1000

    BackEnd and FrontEnd merge

---
+++ BackEnd/src/main/java/com/p107iaq/indoorairquality/user/security/JwtTokenProvider.java
@@ -0,0 +1,70 @@
+package com.p107iaq.indoorairquality.user.security;
+
+import com.p107iaq.indoorairquality.user.model.User;
+import com.p107iaq.indoorairquality.user.repository.UserRepository;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+    @Autowired
+    UserRepository userRepository;
+    //Generate the token
+
+    public String generateToken(String username){
+        //User user = (User)authentication.getPrincipal();
+        User user = userRepository.findByUsername(username);
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime()+ SecurityConstant.EXPIRATION_TIME);
+
+        String userId = Long.toString(user.getId());
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("id", (Long.toString(user.getId())));
+        claims.put("username", user.getUsername());
+        claims.put("fullName", user.getName());
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstant.SECRET)
+                .compact();
+    }
+
+    //Validate the token
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SecurityConstant.SECRET).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        }catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        }catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        }catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        }catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
+
+
+    //Get user Id from token
+
+    public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SecurityConstant.SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
+}
+
---
+++ FrontEnd/src/views/widgets/WidgetsDropdown.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { CRow, CCol, CWidgetStatsB } from '@coreui/react'
+
+const WidgetsDropdown = () => (
+  <CRow>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ color: 'success', value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+    <CCol md={{ span: 3 }}>
+      <CWidgetStatsB
+        progress={{ value: 75 }}
+        text="Widget helper text"
+        title="Widget title"
+        value="89.9%"
+      />
+    </CCol>
+  </CRow>
+)
+
+export default WidgetsDropdown
