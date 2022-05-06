5ec8f541d742c5bd3957eb624ab9c466ae113cd5
Jason <jasonsong.93@gmail.com>
2022-05-06 13:13:40 +1000

    Revert "Code for making HTTP Request to the AWAIR API"
    This reverts commit e1c8147fc1fe5406e0a353045f3df64cebdeaa6f.

--- BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
+++ BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
@@ -10,42 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class IndoorairqualityApplication {
 
-    /**
-     * add this to Maven Dependency
-     * <p>
-     * <!-- https://mvnrepository.com/artifact/com.squareup.okhttp/okhttp -->
-     * <dependency>
-     * <groupId>com.squareup.okhttp</groupId>
-     * <artifactId>okhttp</artifactId>
-     * <version>2.7.5</version>
-     * </dependency>
-     */
-//
-//    OkHttpClient client = new OkHttpClient();
-//    Request req = new Request.Builder()
-//            .addHeader("Authorization", "hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
-//            .url("https://developer-apis.awair.is/v1/orgs/8646")
-//            .build();
-//    Response response = client.newCall(req).execute();
-//        System.out.println(response.body().string());
-
-    public static void main(String[] args) {
-        SpringApplication.run(IndoorairqualityApplication.class, args);
-    }
-
-    @Bean
-    BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(IndoorairqualityApplication.class, args);
+	}
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
