849de800a44f17a301be8cae5135f055017ef0f9
Jason <jasonsong.93@gmail.com>
2022-05-06 13:21:45 +1000

    Added Maven dependency and sample in main class

--- BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
+++ BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
@@ -1,5 +1,8 @@
 package com.p107iaq.indoorairquality;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,19 +10,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class IndoorairqualityApplication {
 
-    //        OkHttpClient client = new OkHttpClient();
-    //        Request req = new Request.Builder()
-    //                .addHeader("Authorization","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
-    //                .url("https://developer-apis.awair.is/v1/orgs/8646")
-    //                .build();
-    //        Response response = client.newCall(req).execute();
-    //        System.out.println(response.body().string());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(IndoorairqualityApplication.class, args);
+
+        OkHttpClient client = new OkHttpClient();
+        Request req = new Request.Builder()
+                .addHeader("Authorization","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
+                .url("https://developer-apis.awair.is/v1/orgs/8646")
+                .build();
+        Response response = client.newCall(req).execute();
+        System.out.println(response.body().string());
+
     }
 
     @Bean
