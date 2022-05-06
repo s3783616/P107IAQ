d5710b47a395bbcd129708106f9e46034ceec0ca
Jason <jasonsong.93@gmail.com>
2022-05-06 13:18:37 +1000

    Added OkHttpClient code

--- BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
+++ BackEnd/src/main/java/com/p107iaq/indoorairquality/IndoorairqualityApplication.java
@@ -10,22 +10,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class IndoorairqualityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IndoorairqualityApplication.class, args);
-	}
-	@Bean
-	BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+    //        OkHttpClient client = new OkHttpClient();
+    //        Request req = new Request.Builder()
+    //                .addHeader("Authorization","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
+    //                .url("https://developer-apis.awair.is/v1/orgs/8646")
+    //                .build();
+    //        Response response = client.newCall(req).execute();
+    //        System.out.println(response.body().string());
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndoorairqualityApplication.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
