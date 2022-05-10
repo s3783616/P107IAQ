8767dc040f235c8da28f4f32eb29dfe26d5a1449
Duc Tran <s3752703@student.rmit.edu.au>
2022-05-11 00:03:44 +1000

    Get raw data, 5min, 15 min is done. Working on 1 minute interval

--- BackEnd/data_services/src/main/java/com/example/data_services/services/DataService.java
+++ BackEnd/data_services/src/main/java/com/example/data_services/services/DataService.java
@@ -1,32 +1,135 @@
 package com.example.data_services.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Service
 public class DataService {
-    String baseURL = "https://developer-apis.awair.is/v1/orgs/8646";
+    private String baseURL = "https://developer-apis.awair.is/v1/orgs/8646";
+    private OkHttpClient client = new OkHttpClient();
+    public String getDevicesList() {
 
-    public String getRawData() {
-
-        OkHttpClient client = new OkHttpClient();
         Request req = new Request.Builder()
                 .addHeader("x-api-key","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
-                .url(baseURL)
+                .url(baseURL+"/devices")
                 .build();
         Response response = null;
+
         {
             try {
                 response = client.newCall(req).execute();
+                return response.body().string();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println(response.body().toString());
-        return response.body().toString();
+        return "";
     }
+
+    public ArrayList<Response> getRawData(String deviceID,String dateFrom, String dateTo){
+        ArrayList<Response> respones = new ArrayList<Response>();
+        LocalDateTime startDate = LocalDateTime.parse(dateFrom);
+        LocalDateTime endDate = LocalDateTime.parse(dateTo);
+
+        while(endDate.compareTo(startDate)>0){
+            LocalDateTime tmp = startDate;
+            tmp = tmp.plusHours(1);
+            if(endDate.compareTo(tmp)<=0) tmp = endDate;
+            String url = baseURL+"/devices/awair-omni/"+deviceID+"/air-data/raw?from="+startDate.toString()+":00.000Z"+"&to="+tmp.toString()+":00.000Z"+"&limit=360&desc=false&fahrenheit=false";
+            Request req = new Request.Builder()
+                    .addHeader("x-api-key","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
+                    .url(url)
+                    .build();
+            Response response = null;
+
+            {
+                try {
+                    response = client.newCall(req).execute();
+                    respones.add(response);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            startDate= tmp;
+        }
+
+        return respones;
+    }
+
+    public ArrayList<Response> get5minutesAvgData(String deviceID,String dateFrom, String dateTo){
+        ArrayList<Response> responses = new ArrayList<Response>();
+        LocalDateTime startDate = LocalDateTime.parse(dateFrom);
+        LocalDateTime endDate = LocalDateTime.parse(dateTo);
+        while(endDate.compareTo(startDate)>0){
+            LocalDateTime tmp = startDate;
+            tmp = tmp.plusHours(24);
+            if(endDate.compareTo(tmp)<=0) tmp = endDate;
+
+            String url = baseURL+"/devices/awair-omni/"+deviceID+"/air-data/5-min-avg?from="+startDate.toString()+":00.000Z"+"&to="+tmp.toString()+":00.000Z"+"&limit=360&desc=false&fahrenheit=false";
+            System.out.println(url);
+            Request req = new Request.Builder()
+                    .addHeader("x-api-key","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
+                    .url(url)
+                    .build();
+            Response response = null;
+
+            {
+                try {
+                    response = client.newCall(req).execute();
+                    responses.add(response);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            startDate = tmp;
+        }
+
+        return responses;
+    }
+    public ArrayList<Response> get15minutesAvgData(String deviceID,String dateFrom, String dateTo){
+        ArrayList<Response> responses = new ArrayList<Response>();
+        LocalDateTime startDate = LocalDateTime.parse(dateFrom);
+        LocalDateTime endDate = LocalDateTime.parse(dateTo);
+
+        while(endDate.compareTo(startDate)>0){
+            LocalDateTime tmp = startDate;
+            tmp = tmp.plusDays(7);
+            if(endDate.compareTo(tmp)<=0) tmp = endDate;
+
+            String url = baseURL+"/devices/awair-omni/"+deviceID+"/air-data/15-min-avg?from="+startDate.toString()+":00.000Z"+"&to="+tmp.toString()+":00.000Z"+"&limit=360&desc=false&fahrenheit=false";
+            Request req = new Request.Builder()
+                    .addHeader("x-api-key","hZqzKsC0SRNY2sXioGnxFGk0srONpcsx")
+                    .url(url)
+                    .build();
+            Response response = null;
+
+            {
+                try {
+                    response = client.newCall(req).execute();
+                    responses.add(response);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            startDate = tmp;
+        }
+
+        return responses;
+    }
+
+
 }
--- BackEnd/data_services/src/main/java/com/example/data_services/web/DataController.java
+++ BackEnd/data_services/src/main/java/com/example/data_services/web/DataController.java
@@ -1,13 +1,16 @@
 package com.example.data_services.web;
-
+import org.json.JSONException;
+import org.json.JSONObject;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.data_services.services.DataService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/data")
@@ -15,10 +18,79 @@ public class DataController {
     @Autowired
     private DataService dataService;
 
-    @GetMapping("")
-    public ResponseEntity<?> getData() {
-        String data = dataService.getRawData();
+    @GetMapping("/list")
+    public ResponseEntity<?>getDeviceList() {
+        String data = dataService.getDevicesList();
 
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/rawData")
+    public ResponseEntity<?>getRawData(@RequestParam(value = "deviceID", required = true) String deviceID,
+                                       @RequestParam(value = "dateFrom", required = true) String dateFrom,
+                                       @RequestParam(value = "dateTo", required = true) String dateTo){
+
+        ArrayList<Response> responses = dataService.getRawData(deviceID,dateFrom,dateTo);
+        String data = "";
+        for (Response response:responses){
+            try {
+                data = data+response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/5minInterval")
+    public ResponseEntity<?>getData5minIntv(@RequestParam(value = "deviceID", required = true) String deviceID,
+                                       @RequestParam(value = "dateFrom", required = true) String dateFrom,
+                                       @RequestParam(value = "dateTo", required = true) String dateTo){
+
+        ArrayList<Response> responses = dataService.get5minutesAvgData(deviceID,dateFrom,dateTo);
+        String data = "";
+        for (Response response:responses){
+            try {
+                data = data+response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/15minInterval")
+    public ResponseEntity<?>getData15minIntv(@RequestParam(value = "deviceID", required = true) String deviceID,
+                                            @RequestParam(value = "dateFrom", required = true) String dateFrom,
+                                            @RequestParam(value = "dateTo", required = true) String dateTo){
+
+        ArrayList<Response> responses = dataService.get15minutesAvgData(deviceID,dateFrom,dateTo);
+        String data = "";
+        for (Response response:responses){
+            try {
+                data = data+response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/1minInterval")
+    public ResponseEntity<?>getData1minIntv(@RequestParam(value = "deviceID", required = true) String deviceID,
+                                            @RequestParam(value = "dateFrom", required = true) String dateFrom,
+                                            @RequestParam(value = "dateTo", required = true) String dateTo){
+        ArrayList<Response> responses = dataService.getRawData(deviceID,dateFrom,dateTo);
+        String data= "";
+        for (Response response:responses){
+            try {
+                JSONObject jsonObject = new JSONObject(response.body().string());
+                System.out.println("JSON Object: "+jsonObject.getJSONObject("data"));
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 }
