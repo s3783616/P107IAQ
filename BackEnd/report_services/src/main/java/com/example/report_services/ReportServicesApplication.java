6d72562728dbaa9b2253cff736427a9623ec09bf
Duc Tran <s3752703@student.rmit.edu.au>
2022-05-17 21:00:29 +1000

    Rework one minute avg data endpoint. Each entry is now the avg of every parameter over 1 minute

---
+++ BackEnd/report_services/src/main/java/com/example/report_services/ReportServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.report_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportServicesApplication.class, args);
+    }
+
+}
---
+++ BackEnd/report_services/src/test/java/com/example/report_services/ReportServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.report_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
