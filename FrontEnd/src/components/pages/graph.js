2f29d80cb064d1f2593f859936013ea553123e47
Thomas Jukes <s3783616@student.rmit.edu.au>
2022-05-18 15:29:19 +1000

    Did a quick example graph to see if things worked

--- FrontEnd/src/components/pages/graph.js
+++ FrontEnd/src/components/pages/graph.js
@@ -3,15 +3,38 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Chart from "chart.js/auto"; //Importing graphing function
 const chart = new Chart(ctx, {
-  type: "line",
-  data: data, //This should be gathered from the JSON at some point
-  options: {
-    onClick: (e) => {
-      const canvasPosition = getRelativePosition(e, chart);
-
-      // Substitute the appropriate scale IDs
-      const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-      const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+  type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'particles sensed',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
-  },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+        
+    }
 });
+ //This should be gathered from the JSON at some point
