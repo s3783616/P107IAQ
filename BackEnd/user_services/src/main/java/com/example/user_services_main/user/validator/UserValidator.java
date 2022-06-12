1242f02f63707c824ab07a74b7b4cc842ef92a25
Jason <jasonsong.93@gmail.com>
2022-06-12 21:10:07 +1000

    Cleaned up file and formatting

--- BackEnd/user_services/src/main/java/com/example/user_services_main/user/validator/UserValidator.java
+++ BackEnd/user_services/src/main/java/com/example/user_services_main/user/validator/UserValidator.java
@@ -15,21 +15,12 @@ public class UserValidator implements Validator {
 
     @Override
     public void validate(Object object, Errors errors) {
-
         User user = (User) object;
-
-        if(user.getPassword().length() <6){
-            errors.rejectValue("password","Length", "Password must be at least 6 characters");
+        if (user.getPassword().length() < 6) {
+            errors.rejectValue("password", "Length", "Password must be at least 6 characters");
         }
-
-        if(!user.getPassword().equals(user.getConfirmPassword())){
-            errors.rejectValue("confirmPassword","Match", "Passwords must match");
-
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
+            errors.rejectValue("confirmPassword", "Match", "Passwords must match");
         }
-
-        //confirmPassword
-
-
-
     }
 }
