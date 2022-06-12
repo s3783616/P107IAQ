09b23565a53c21de28bb3a9959989d402732e7a5
Jason <jasonsong.93@gmail.com>
2022-06-12 20:37:47 +1000

    Removed extra request parameter that wasn't being used, cleaned up file

--- BackEnd/user_services/src/main/java/com/example/user_services_main/user/exceptions/CustomResponseEntityExceptionHandler.java
+++ BackEnd/user_services/src/main/java/com/example/user_services_main/user/exceptions/CustomResponseEntityExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request){
+    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex) {
         UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
